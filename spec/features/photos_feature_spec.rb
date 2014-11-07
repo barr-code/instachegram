1be973c825a71045d864d24f84640242163447f7
Victoria Barr <vb329@georgetown.edu>
2014-11-07 18:32:27 +0000

    Photos have captions, which are displayed on the webpage; installed gem paperclip; time to figure out how to implement that

--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -8,7 +8,19 @@ describe 'photos' do
 			visit '/photos'
 			expect(page).to have_content 'No photos (yet).'
 			expect(page).to have_link 'Add a photo'
+		end
+	end
 
+	context 'there are photos' do
+
+		before do
+			Photo.create(caption: "#nofilter")
+		end
+
+		it 'displays photos' do
+			visit '/photos'
+			expect(page).to have_content "#nofilter"
+			expect(page).not_to have_content "No photos (yet)."
 		end
 	end
 end
