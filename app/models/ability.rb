0fed5ffe07397229dd9f678f57e48a394e797ab1
Victoria Barr <vb329@georgetown.edu>
2014-11-09 17:43:05 +0000

    Users can like photos

---
+++ app/models/ability.rb
@@ -0,0 +1,13 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    if user.nil?
+      can :read, :all
+    else
+      can :read, :all
+      can :create, Photo
+      can [:update, :destroy], Photo, :user_id => user.id
+    end
+  end
+end
---
+++ spec/features/likes_feature_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+require_relative './helpers/photo_helper'
+require_relative './helpers/user_helper'
+
+describe 'liking photos' do
+
+	before do
+		sign_up
+		upload_photo
+	end
+
+	it 'users can like photos, update like count', js: true do
+		visit '/'
+		click_link 'Like'
+		expect(page).to have_content '1 like'
+	end
+
+end
