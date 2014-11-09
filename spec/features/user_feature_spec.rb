d0a1f113f522af86cda04eb134f26b1d96bf804c
Victoria Barr <vb329@georgetown.edu>
2014-11-09 12:15:54 +0000

    Users can sign in and sign out

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -14,3 +14,30 @@ context 'user not signed in' do
 	end
 
 end
+
+context 'user is signed in' do
+
+	before do
+		visit '/'
+		click_link 'Sign up'
+		fill_in 'Email', with: 'test@user.com'
+		fill_in 'Password', with: 'testpass'
+		fill_in 'Password confirmation', with: 'testpass'
+		click_button 'Sign up'
+	end
+
+	it 'has a "sign out" link' do
+		visit '/'
+		expect(page).to have_link 'Sign out'
+	end
+
+	it 'does not have a "sign up" link' do
+		visit '/'
+		expect(page).not_to have_link 'Sign up'
+	end
+
+	it 'does not have a "sign in" link' do
+		visit '/'
+		expect(page).not_to have_link 'Sign in'
+	end
+end
