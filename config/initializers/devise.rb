874891b1e2f659a30c10fe8c8001616ff0ea904f
Victoria Barr <vb329@georgetown.edu>
2014-11-17 20:15:08 +0000

    Temporarily got rid of Facebook sign in

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -22,7 +22,7 @@ Devise.setup do |config|
   # available as additional gems.
   require 'devise/orm/active_record'
 
-  config.omniauth :facebook, Rails.application.secrets.FACEBOOK_APP_ID, Rails.application.secrets.FACEBOOK_APP_SECRET
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
